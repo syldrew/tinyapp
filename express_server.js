31ee92ab879234c0dde577ff7a0011b4d426935e
syldrew <syl_peke14@hotmail.com>
2023-06-25 09:46:12 -0500

    Refactoring MidTerm project

--- express_server.js
+++ express_server.js
@@ -1,54 +1,17 @@
 const express = require("express");
-const cookieSession = require("cookie-session");
+const bodyParser = require("body-parser");
+const cookieSession = require('cookie-session');
+
 const app = express();
 const PORT = 8080; 
 
 
-
-
-
-
-//************************************************************************************************* 
-//Checks if current cookie corresponds with a user in the userDatabase 
-
-const cookieHasUser = function(cookie, userDatabase) {
-    for (const user in userDatabase) {
-      if (cookie === user) {
-        return true;
-      }
-    } return false;
-  };
-
-//Generate a Random Short URL ID function
-
-function generateRandomString(length) {
-    return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
-}
-console.log(generateRandomString(6));
-
-
-/* Returns an object of short URLs specific to the passed in userID */
-const urlsForUser = function(id, urlDatabase) {
-    const userUrls = {};
-    for (const shortURL in urlDatabase) {
-      if (urlDatabase[shortURL].userID === id) {
-        userUrls[shortURL] = urlDatabase[shortURL];
-      }
-    }
-    return userUrls;
-  };
-
-//**************************************************************************************************
-
-
 const urlDatabase = { 
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+    "b2xVn2": "http://www.lighthouselabs.ca",
+    "9sm5xK": "http://www.google.com"
+  };
 
-
-//Create a users Object 
-const users = {
+  const users = {
     userRandomID: {
       id: "userRandomID",
       email: "user@example.com",
@@ -64,16 +27,43 @@ const users = {
 
 app.set("view engine", "ejs");
 
-//Getting Ready for POST Requests
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(cookieSession({
-    name: 'session',
-    keys: ['SILVIA'],
-    maxAge: 24 * 60 * 60 * 1000,
-  }));
+  name: 'session',
+  secret: 'secret'
+}));
 
-                                                    //****GETS ****/
+// FUNCTIONS IMPLEMENTATION **********************************************************************************
+
+function generateRandomString(length) {
+    return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
+}
+console.log(generateRandomString(6));
+
+
+const cookieHasUser = function(cookie, userDatabase) {
+    for (const user in userDatabase) {
+      if (cookie === user) {
+        return true;
+      }
+    } return false;
+  };
+
+const urlsForUser = function(id, urlDatabase) {
+    const userUrls = {};
+    for (const shortURL in urlDatabase) {
+      if (urlDatabase[shortURL].userID === id) {
+        userUrls[shortURL] = urlDatabase[shortURL];
+      }
+    }
+    return userUrls;
+  };
+
+//***********************************************************************************************************/
+                                                   // Express and Routes
+
+
 
 
 app.get("/", (req, res) => {
@@ -84,9 +74,8 @@ app.get("/", (req, res) => {
     }
   });
   
-
-
-  app.get("/urls", (req, res) => {
+  
+app.get("/urls", (req, res) => {
     let templateVars = {
       urls: urlsForUser(req.session.user_id, urlDatabase),
       user: users[req.session.user_id],
@@ -95,18 +84,7 @@ app.get("/", (req, res) => {
   });
 
 
-
-  app.get("/urls", (req, res) => {
-    const templateVars = {
-      username: req.cookies["username"],
-      // ... any other vars
-    };
-    res.render("urls_index", templateVars);
-  });  
-
-
-  //Add a GET Route to Show the Form
-  app.get("/urls/new", (req, res) => {
+app.get("/urls/new", (req, res) => {
     if (!cookieHasUser(req.session.user_id, users)) {
       res.redirect("/login");
     } else {
@@ -116,9 +94,8 @@ app.get("/", (req, res) => {
       res.render("urls_new", templateVars);
     }
   });
-  
 
-//Create a Registration Page. Create a GET /register endpoint, which returns the template you just created. 
+
 app.get("/register", (req, res) => {
     if (cookieHasUser(req.session.user_id, users)) {
       res.redirect("/urls");
@@ -130,7 +107,8 @@ app.get("/register", (req, res) => {
     }
   });
 
-  app.get("/login", (req, res) => {
+
+app.get("/login", (req, res) => {
     if (cookieHasUser(req.session.user_id, users)) {
       res.redirect("/urls");
     } else {
@@ -140,11 +118,7 @@ app.get("/register", (req, res) => {
       res.render("urls_login", templateVars);
     }
   });
-  
 
-
-
-//Adding a Second Route and Template
 app.get("/urls/:shortURL", (req, res) => {
     if (urlDatabase[req.params.shortURL]) {
       let templateVars = {
@@ -160,10 +134,6 @@ app.get("/urls/:shortURL", (req, res) => {
   });
 
 
-
-
-
-//urlDatabase[req.params.shortURL].longURL;
 app.get("/u/:shortURL", (req, res) => {
     if (urlDatabase[req.params.shortURL]) {
       const longURL = urlDatabase[req.params.shortURL].longURL;
@@ -176,19 +146,11 @@ app.get("/u/:shortURL", (req, res) => {
       res.status(404).send("The short URL you are trying to access does not correspond with a long URL at this time.");
     }
   });
-  
 
 
+//********************************************* */
 
-
-
-                                                          //****POSTS ****/
-
-
-
-
-//Redirect After Form Submission
-app.post("/urls", (req, res) => {
+  app.post("/urls", (req, res) => {
     if (req.session.user_id) {
       const shortURL = generateRandomString();
       urlDatabase[shortURL] = {
@@ -200,19 +162,36 @@ app.post("/urls", (req, res) => {
       res.status(401).send("You must be logged in to a valid account to create short URLs.");
     }
   });
-  
-  
 
 
-//Add an endpoint to handle a POST to /login in your Express server.
+  app.post("/register", (req, res) => {
+    const newUserID = generateRandomString();
+    users[newUserID] = {
+      id: newUserID,
+      email: "user@example.com",
+      password: "purple-monkey-dinosaur",
+    };
+    req.session.user_id = newUserID;
+    res.redirect("/urls");
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+    
  app.post("/logout", (req, res) => {
         req.session = null;
         res.redirect('/urls');
       });
 
-
-
-//Add a POST route that removes a URL resource: POST /urls/:id/delete
 
 app.post("/urls/:shortURL/delete", (req, res) => {
     const userID = req.session.user_id;
@@ -226,9 +205,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
     }
   });
 
-//Add a POST route that updates a URL resource; POST /urls/:id and have it update the value of your
-// stored long URL based on the new value in req.body.
-// Finally, redirect the client back to /urls.
+
 app.post("/urls/:id", (req, res) => {
     const userID = req.session.user_id;
     const userUrls = urlsForUser(userID, urlDatabase);
@@ -241,9 +218,8 @@ app.post("/urls/:id", (req, res) => {
     }
   });
 
-  app.get("/", (req, res) => { //*********/
-    res.send("Hello!");
-    });
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
@@ -258,39 +234,3 @@ app.listen(PORT, () => {
 
 
 
-//   app.get("/urls/:id", (req, res) => {
-//     const templateVars = { id: req.params.id, longURL: "http://www.lighthouselabs.ca"};
-//     res.render("urls_show", templateVars);
-//   });
-
-
-// //Redirect any request to "/u/:id" to its longURL
-// app.get("/u/:id", (req, res) => {
-//     const longURL = "http://www.lighthouselabs.ca";
-//     res.redirect(longURL);
-//   });
-
-
-//   app.get("/hello", (req, res) => {
-//     res.send("<html><body>Hello <b>World</b></body></html>\n");
-//   });
-
-
-//Sending Data to urls_index.ejs
-
-//   app.get("/urls", (req, res) => {
-//     const templateVars = { urls: urlDatabase };
-//     res.render("urls_index", templateVars);
-//   });
-
-
-  //Add a POST Route to Receive the Form Submission
-//   app.post("/urls", (req, res) => {
-//     console.log(req.body); // Log the POST request body to the console
-//     res.send("Ok"); // Respond with 'Ok' (we will replace this)
-//   });
-
-//app.post("/login", (req, res) => {
-//     req.session.user_id = userID;
-//     res.redirect("/urls");
-//       });
