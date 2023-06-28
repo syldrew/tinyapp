6aec156af83a4f27222c4e52be16b96919c090a9
syldrew <syl_peke14@hotmail.com>
2023-06-28 12:03:11 -0500

    Refactoring of functions and creation of helpers file

---
+++ helpers.js
@@ -0,0 +1,54 @@
+
+// Checks if given email corresponds to a user in a given database, returns true or false //
+const emailHasUser = function(email, userDatabase) {
+    for (const user in userDatabase) {
+      if (userDatabase[user].email === email) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  //Takes an email and userDatabase and returns the user ID for the user with the given email address //
+const userIdFromEmail = function(email, userDatabase) {
+    for (const user in userDatabase) {
+      if (userDatabase[user].email === email) {
+        return userDatabase[user].id;
+      }
+    }
+  };
+
+// Generates a random string, used for creating short URLs and userIDs//
+ function generateRandomString(length) {
+    return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
+}
+
+
+//Checks if current cookie corresponds with a user in the userDatabase//
+const cookieHasUser = function(cookie, userDatabase) {
+    for (const user in userDatabase) {
+      if (cookie === user) {
+        return true;
+      }
+    } return false;
+  };
+
+
+  // Returns an object of short URLs specific to the passed in userID//
+const urlsForUser = function(id, urlDatabase) {
+    const userUrls = {};
+    for (const shortURL in urlDatabase) {
+      if (urlDatabase[shortURL].userID === id) {
+        userUrls[shortURL] = urlDatabase[shortURL];
+      }
+    }
+    return userUrls;
+  };
+
+  module.exports = {
+    generateRandomString,
+    emailHasUser,
+    userIdFromEmail,
+    urlsForUser,
+    cookieHasUser
+  };
