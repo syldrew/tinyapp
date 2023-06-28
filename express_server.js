a27dc14dde7db49e7f1a0b6689d42dfa0a53f357
syldrew <syl_peke14@hotmail.com>
2023-06-28 12:41:22 -0500

    export functions into helpers.js file

--- express_server.js
+++ express_server.js
@@ -9,27 +9,9 @@ const PORT = 8080;
 const { generateRandomString, emailHasUser, userIdFromEmail, urlsForUser, cookieHasUser } = require("./helpers");
 
 const urlDatabase = { 
-    b6UTxQ: {
-        longURL: "https://www.tsn.ca",
-        userID: "aJ48lW",
-      },
-      i3BoGr: {
-        longURL: "https://www.google.ca",
-        userID: "aJ48lW",
-      },
   };
 
-  const users = {
-    userRandomID: {
-      id: "userRandomID",
-      email: "user@example.com",
-      password: "purple-monkey-dinosaur",
-    },
-    user2RandomID: {
-      id: "user2RandomID",
-      email: "user2@example.com",
-      password: "dishwasher-funk",
-    },
+  const users = {   
   };
   
 
