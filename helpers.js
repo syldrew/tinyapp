818266bd59e357d38286fc3ec06ba383b6fa3c3c
syldrew <syl_peke14@hotmail.com>
2023-07-04 12:48:06 -0500

    Removing all Commented code

--- helpers.js
+++ helpers.js
@@ -1,5 +1,3 @@
-
-// Checks if given email corresponds to a user in a given database, returns true or false //
 const getUserByEmail = function(email, userDatabase) {
     for (const user in userDatabase) {
       if (userDatabase[user].email === email) {
@@ -9,7 +7,6 @@ const getUserByEmail = function(email, userDatabase) {
     return undefined;
   };
 
-  //Takes an email and userDatabase and returns the user ID for the user with the given email address //
 const userIdFromEmail = function(email, userDatabase) {
     for (const user in userDatabase) {
       if (userDatabase[user].email === email) {
@@ -18,7 +15,6 @@ const userIdFromEmail = function(email, userDatabase) {
     }
   };
 
-// Generates a random string, used for creating short URLs and userIDs//
 function generateRandomString() {
     let genRes = '';
     const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -29,8 +25,6 @@ function generateRandomString() {
     return genRes;
   };
 
-
-//Checks if current cookie corresponds with a user in the userDatabase//
 const cookieHasUser = function(cookie, userDatabase) {
     for (const user in userDatabase) {
       if (cookie === user) {
@@ -39,8 +33,6 @@ const cookieHasUser = function(cookie, userDatabase) {
     } return false;
   };
 
-
-  // returns the URLs where the userID is equal to the id of the currently logged-in user.//
 const urlsForUser = function(id, urlDatabase) {
     const userUrls = {};
     for (const shortURL in urlDatabase) {
