d6a515680025134f2cf5f7352bf6a181a123f483
syldrew <syl_peke14@hotmail.com>
2023-07-09 19:06:27 -0500

    Added database file

---
+++ database.js
@@ -0,0 +1,27 @@
+const urlDatabase = {
+    "b2xVn2": {
+        longUrl: "http://www.lighthouselabs.ca",
+        userID: "userRandomID"
+      },
+      "9sm5xK": {
+        longUrl: "http://www.google.com",
+        userID: "userRandomID"
+      },
+  };
+  const users = {
+    userRandomID: {
+      id: "userRandomID",
+      email: "user@example.com",
+      password: "purple-monkey-dinosaur",
+    },
+    user2RandomID: {
+      id: "user2RandomID",
+      email: "user2@example.com",
+      password: "dishwasher-funk",
+    },
+  };
+
+  module.exports = {
+    urlDatabase,
+    users
+  };
