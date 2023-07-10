7f4c88af59d0fb52c8b59d8239f8ae5f84813e30
syldrew <syl_peke14@hotmail.com>
2023-07-09 19:05:51 -0500

    Added database file

--- express_server.js
+++ express_server.js
@@ -7,30 +7,8 @@ const app = express();
 const PORT = 8080;  
 
 const { generateRandomString, getUserByEmail, userIdFromEmail, urlsForUser, cookieHasUser } = require("./helpers");
+const { urlDatabase, users } = require("./database");
 
-const urlDatabase = {
-    "b2xVn2": {
-        longUrl: "http://www.lighthouselabs.ca",
-        userID: "userRandomID"
-      },
-      "9sm5xK": {
-        longUrl: "http://www.google.com",
-        userID: "userRandomID"
-      },
-  };
-  const users = {
-    userRandomID: {
-      id: "userRandomID",
-      email: "user@example.com",
-      password: "purple-monkey-dinosaur",
-    },
-    user2RandomID: {
-      id: "user2RandomID",
-      email: "user2@example.com",
-      password: "dishwasher-funk",
-    },
-  };
-  
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({extended: true}));
